84d5e354bf0708c2d42d580747afc2de1ea8b223
Marian Noekel <marian@MacCarlo.fritz.box>
2024-05-04 00:17:19 +0200

    fix build issue with authentication

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,16 +1,8 @@
-
 import NextAuth from "next-auth";
-import GitHubProvider from "next-auth/providers/github";
+import authOptions from "../../../../auth.config"; 
 
-export const authOptions = {
-  providers: [
-    GitHubProvider({
-      clientId: process.env.GITHUB_ID ?? "",
-      clientSecret: process.env.GITHUB_SECRET ?? "",
-    }),
-  ],
-};
 
-export const handler = NextAuth(authOptions);
+
+const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
---
+++ auth.config.ts
@@ -0,0 +1,13 @@
+
+import GitHubProvider from "next-auth/providers/github";
+
+const authOptions = {
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID ?? "",
+      clientSecret: process.env.GITHUB_SECRET ?? "",
+    }),
+  ],
+};
+
+export default authOptions
